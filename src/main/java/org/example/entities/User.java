bb641e1965b9c17554bb31f409345bb1856f51cf
barilskagg <gergana.barilska@scalefocus.com>
2020-01-26 13:22:24 +0200

    Initial commit

--- src/main/java/org/example/entities/User.java
+++ src/main/java/org/example/entities/User.java
@@ -4,42 +4,62 @@ import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
-@Table(name = "users")
+@Table(name = "user")
 @Entity
 public class User {
     @Column
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
     @NotEmpty
     @Column
-    private String email;
+    private String userName;
     @Size(min = 4, max = 20)
     @NotEmpty
     @Column
     private String password;
+    @Column
+    private Boolean active;
+    @Column
+    private String roles;
 
-    public void setId(Long id) {
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setId(Integer id) {
         this.id = id;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public Long getId() {
-        return id;
+    public void setActive(boolean active) {
+        this.active = active;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
+    public void setRoles(String roles) {
+        this.roles = roles;
     }
 }
