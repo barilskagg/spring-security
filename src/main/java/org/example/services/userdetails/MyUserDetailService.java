e1e2385c43299a820c751fc1c73abd0340557751
barilskagg <gergana.barilska@scalefocus.com>
2020-01-26 13:43:20 +0200

    Revert "Revert "Initial commit""
    This reverts commit 510043bd573476b23825b8d67b80fe345402e5b5.

---
+++ src/main/java/org/example/services/userdetails/MyUserDetailService.java
@@ -0,0 +1,25 @@
+package org.example.services.userdetails;
+
+import org.example.entities.User;
+import org.example.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(userName);
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: "+userName));
+        return user.map(MyUserDetails::new).get();
+    }
+}
